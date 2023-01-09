eabcebf017f9fe0026f3a132b7c10eec94916c67
seungjae93 <lsj72023892@gmail.com>
2023-01-09 21:16:37 +0900

    :tada: app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import Router from "./shared/Router";
+import Router from "react-router";
 import GlobalStyle from "./components/styles/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Router />
+      </QueryClientProvider>
     </>
   );
 }
-
 export default App;
