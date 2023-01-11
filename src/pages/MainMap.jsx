84c1ffcdbb435316b92499ece367bd4a60c7a9d3
seungjae93 <lsj72023892@gmail.com>
2023-01-11 15:41:26 +0900

    sd

--- src/pages/MainMap.jsx
+++ src/pages/MainMap.jsx
@@ -160,22 +160,22 @@ const MainMap = () => {
               height: "100%",
             }}
             level={3} // 지도의 확대 레벨
-            onDragEnd={(map) =>
-              setPos({
-                center: {
-                  lat: map.getCenter().getLat(),
-                  lng: map.getCenter().getLng(),
-                },
-                swLatLng: {
-                  lat: map.getBounds().getSouthWest().getLat(),
-                  lng: map.getBounds().getSouthWest().getLng(),
-                },
-                neLatLng: {
-                  lat: map.getBounds().getNorthEast().getLat(),
-                  lng: map.getBounds().getNorthEast().getLng(),
-                },
-              })
-            }
+            // onDragEnd={(map) =>
+            //   setPos({
+            //     center: {
+            //       lat: map.getCenter().getLat(),
+            //       lng: map.getCenter().getLng(),
+            //     },
+            //     swLatLng: {
+            //       lat: map.getBounds().getSouthWest().getLat(),
+            //       lng: map.getBounds().getSouthWest().getLng(),
+            //     },
+            //     neLatLng: {
+            //       lat: map.getBounds().getNorthEast().getLat(),
+            //       lng: map.getBounds().getNorthEast().getLng(),
+            //     },
+            //   })
+            // }
             onIdle={handleMapInfo}
           >
             {positions.map((position) => {
